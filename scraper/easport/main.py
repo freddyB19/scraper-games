fe468a50486800f1f1a0694168d95126771d5750
freddyB19 <freddyybolivar2013@gmail.com>
2024-10-27 11:35:53 -0400

    agregando descarga y lector de pagina

--- scraper/easport/main.py
+++ scraper/easport/main.py
@@ -8,6 +8,7 @@ from lxml import html
 
 from bs4 import BeautifulSoup
 
+from utils.main import ReadFromFile
 
 URL:Dict[str, str] = {
 	'easport': 'https://www.ea.com/es-es',
@@ -18,9 +19,37 @@ URL:Dict[str, str] = {
 	'gratuitos': 'https://www.ea.com/es-es/games/library/freetoplay'
 }
 
+BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class ScraperEASport:
+
+	@classmethod
+	def scraper(cls):
+		path = os.path.join(BASE, 'data', 'easport', 'easport.html')
+		data = ReadFromFile.read(path)
+
+		html_parsed = BeautifulSoup(data, 'lxml')
+
+		container = html_parsed.find('ea-box-set', unresolved=True, attrs={'spacing-top': 'none'})
+
+		for destacados in container.find_all('ea-container', slot='container'):
+			print(destacados['ea-game-box '].get('background-image'))
+
+
+
+	@classmethod
+	def download(cls):
+		response = httpx.get(URL['easport'])
+		
+		if response.status_code == 200:
+			page = html.fromstring(response.text)
+			html_parsed = BeautifulSoup(html.tostring(page), 'lxml')
+
+			with open(os.path.join(BASE, 'data', 'easport', 'easport.html'), 'a') as file:
+				file.write(html_parsed.prettify())
+
 
 def main():
-	pass
+	ScraperEASport.scraper()
 
 
 if __name__ == '__main__':
