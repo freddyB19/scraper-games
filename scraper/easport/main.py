b0c80782b0fcb69ccab958add4da227c476adf9b
freddyB19 <freddyybolivar2013@gmail.com>
2024-10-27 13:00:04 -0400

    agregando scraper noticias easport

--- scraper/easport/main.py
+++ scraper/easport/main.py
@@ -23,9 +23,29 @@ URL:Dict[str, str] = {
 }
 
 class ScraperEASport:
+	PATH = os.path.join(BASE, 'data', 'easport')
+
+	@classmethod
+	def noticias(cls):
+		data = ReadFromFile.read(os.path.join(cls.PATH, 'noticias.html'))
+		html_parsed = BeautifulSoup(data, 'lxml')
+
+		container = html_parsed.find('ea-grid')
+
+		for noticia in container.css.select('ea-container  ea-tile[slot="tile"]'):
+			print(f"Imagen: {noticia.get('media')}")
+			print(f"Titulo: {noticia.get('title-text')}")
+			extra = noticia.find_all('div')
+			print(f"Tag: {extra[0].string.strip()} -- Fecha: {extra[1].string.strip()}")
+			print(f"Descripcion: {noticia.find('ea-tile-copy').string.strip()}")
+			print()
+	
 
 	@classmethod
 	def scraper(cls):
+		cls.noticias()
+
+		"""
 		path = os.path.join(BASE, 'data', 'easport', 'easport.html')
 		data = ReadFromFile.read(path)
 
@@ -33,20 +53,24 @@ class ScraperEASport:
 
 		container = html_parsed.find('ea-box-set', unresolved=True, attrs={'spacing-top': 'none'})
 
+		print("Juegos destacados")
 		for destacados in container.find_all('ea-container', slot='container'):
-			print(destacados.find('ea-game-box').get('main-link-title'))
-
-
+			juego = destacados.find('ea-game-box')
+			print(juego.get('main-link-title'))
+			print(juego.get('background-image'))
+			print(f"{'https://www.ea.com'}{juego.get('main-link-url')}")
+			print()
+		"""
 
 	@classmethod
 	def download(cls):
-		response = httpx.get(URL['easport'])
+		response = httpx.get(URL['gratuitos'])
 		
 		if response.status_code == 200:
 			page = html.fromstring(response.text)
 			html_parsed = BeautifulSoup(html.tostring(page), 'lxml')
 
-			with open(os.path.join(BASE, 'data', 'easport', 'easport.html'), 'a') as file:
+			with open(os.path.join(BASE, 'data', 'easport', 'gratuitos.html'), 'a') as file:
 				file.write(html_parsed.prettify())
 
 
