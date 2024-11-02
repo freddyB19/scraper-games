d152efb606c7789f6d15fbe84335abf49e552c4c
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-02 00:14:25 -0400

    feat: scraper elnacional_tecnologia a noticias main

--- scraper/noticias/main.py
+++ scraper/noticias/main.py
@@ -22,27 +22,70 @@ URL = {
 
 
 class ScraperNoticias:
+	PATH = os.path.join(BASE, 'data', 'noticias')
+	
+	@classmethod
+	def elnacional_tecnologia(cls):
+		html_parsed = ReadFromFile.read(os.path.join(cls.PATH, 'elnacional_tecnologia.html'))
+
+		if html_parsed is not None:
+			contenido_principal = html_parsed.find('div', class_ = 'module-category')
+			url =  contenido_principal.a.get('href')
+			titulo =  contenido_principal.a.get('title')
+			categoria = contenido_principal.find('div', class_='category').string.strip()
+			fecha = contenido_principal.find('time').string.strip()
+			no, protocolo, ruta = contenido_principal.find('div', class_='background-image').get('style').partition("https://")
+			imagen = protocolo + ruta
+
+			print(" +--- Noticia Principal ---+")
+			print(f"[+] Url : {url}")
+			print(f"[+] Titulo : {titulo}")
+			print(f"[+] Categoria : {categoria}")
+			print(f"[+] Fecha : {fecha}")
+			print(f"[+] Imagen : {imagen}")
+			print(" +--- --------------- ---+")
+
+			for articulo in html_parsed.find_all('div', class_ = 'article'):
+				imagen = articulo.css.select('div[class="image"] a img')[0].get("src")
+				url = articulo.css.select('div[class="image"] a')[0].get("href")
+				titulo = articulo.css.select('div[class="title"] a h2')[0].string.strip()
+				meta = articulo.css.select('div[class="meta"] a[class="category"]')[0].get('title')
+				fecha = articulo.css.select('div[class="meta"] time')[0].string.strip()
+				extra = articulo.css.select('div[class="extract"] a')[0].string.strip()
+
+				print(f"...[-] Imagen: {imagen}")
+				print(f"...[-] Url: {url}")
+				print(f"...[-] Titulo: {titulo}")
+				print(f"...[-] Meta: {meta}")
+				print(f"...[-] fecha: {fecha}")
+				print(f"...[-] Extra: {extra}")
+
+				print("-----------------")
+
+				print()
+
+
+		else:
+			print(f"[-] Error: {html_parsed}")
+
 	@classmethod
 	def scraper(cls):
-		pass
+		cls.elnacional_tecnologia()
 
 	@classmethod 
 	def download(cls):
-		PATH = os.path.join(BASE, 'data', 'noticias')
 		
 		for web in URL.keys():
 			html_parsed = ReadFromWeb.read(url = URL[web])
 
 			if html_parsed:
-				with open(os.path.join(PATH, f"{web}.html"), 'a') as file:
+				with open(os.path.join(cls.PATH, f"{web}.html"), 'a') as file:
 					file.write(html_parsed.prettify())
 			else:
 				print(f"[-] Error: {html_parsed}, Path: {web}")
 
 def main():
-	ScraperNoticias.download()
+	ScraperNoticias.scraper()
 
 if __name__ == '__main__':
 	main()
-
-# git branch -c | -C albion noticias
