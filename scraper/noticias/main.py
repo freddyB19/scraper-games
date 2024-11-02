89c26b7a7780a2e90e48c115e1f9d4d6bf0e4958
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-02 18:00:29 -0400

    feat: scraper decrypt a noticias main

--- scraper/noticias/main.py
+++ scraper/noticias/main.py
@@ -70,9 +70,80 @@ class ScraperNoticias:
 		else:
 			print(f"[-] Error: {html_parsed}")
 
+	
+
+	@classmethod
+	def decrypt(cls):
+		#html_parsed = ReadFromFile.read(os.path.join(cls.PATH, 'decrypt.html'))
+		html_parsed = ReadFromFile.read(os.path.join(cls.PATH, 'decrypt_ia.html'))
+
+
+		if html_parsed is not None:
+			noticia_principal = html_parsed.find('article', class_ = 'h-full')
+
+			img = noticia_principal.find('img').get('src')
+			categoria = noticia_principal.css.select('a div[class*="text-cc-pink-2"] span')[0].string.strip()
+			titulo = noticia_principal.css.select(
+				'h3[class*="font-akzidenz-grotes"] > a[class*="linkbox__overlay"]'
+			)[0].string.strip()
+			url = noticia_principal.css.select(
+				'h3[class*="font-akzidenz-grotes"] > a[class*="linkbox__overlay"]'
+			)[0].get('href')
+			descripcion = noticia_principal.css.select('p[class*="mt-1 gg-dark:text-neutral-100"]')[0].string.strip()
+			fecha = noticia_principal.css.select('footer div[class="mt-2 md:mt-4"] time')[0].string.strip()
+
+
+			print(f"[+] Imagen: {img}")
+			print(f"[+] Categoria: {categoria}")
+			print(f"[+] Titulo: {titulo}")
+			print(f"[+] Url: {url}")
+			print(f"[+] Descripcion: {descripcion}")
+			print(f"[+] Fecha: {fecha}")
+
+
+			for articulo in html_parsed.css.select('article[class="w-full"] article[class="linkbox flex space-x-3"]'):
+				categoria = articulo.find('p', class_='text-cc-pink-2').string.strip()
+				url = articulo.find('a', class_='linkbox__overlay').get('href')
+				titulo = [
+					articulo.find('span', class_="bitcoin:group-hover:bg-orange-400").string.strip()
+					if articulo.find('span', class_="bitcoin:group-hover:bg-orange-400")
+					else None
+				][0]
+				fecha =  articulo.css.select('time[class!="inline-flex items-center gap-x-1"]')[0].string.strip()
+				meta = articulo.css.select('time[class*="inline-flex"] span')[0].string.strip()
+
+				print(f"\t[--] Categoria: {categoria}")
+				print(f"\t[--] Url: {url}")
+				print(f"\t[--] Titulo: {titulo}")
+				print(f"\t[--] Fecha : {fecha}")
+				print(f"\t[--] Meta : {meta}")
+
+				print("\n\n")
+
+			for articulo in html_parsed.css.select('article[class*="max-w-[764px]"] > article'):
+				fecha = articulo.css.select('h4')[0].string.strip()
+
+				categoria = articulo.find('p', class_ = "text-cc-pink-2").string.strip()
+				url = articulo.find('a', class_="linkbox__overlay").get('href')
+				titulo = articulo.css.select('a[class="linkbox__overlay"] span[class="font-medium"]')[0].string.strip()
+				descripcion = articulo.find('p', class_="gg-dark:text-neutral-100").string.strip()
+				meta = articulo.css.select('footer p[class*="flex"] time > span')[0].string.strip()
+
+				print(f"\t[---] Fecha : {fecha}")
+				print(f"\t[---] Categoria : {categoria}")
+				print(f"\t[---] Url : {url}")
+				print(f"\t[---] Titulo : {titulo}")
+				print(f"\t[---] Descripcion : {descripcion}")
+				print(f"\t[---] Meta : {meta}")
+
+				print("\n\n")
+
+
 	@classmethod
 	def scraper(cls):
-		cls.elnacional_tecnologia()
+		#cls.elnacional_tecnologia()
+		cls.decrypt()
+
 
 	@classmethod 
 	def download(cls):
