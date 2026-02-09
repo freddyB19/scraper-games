b622e31635bbc5211ec0c41962c14e9a1b6ade65
freddyB19 <freddyybolivar2013@gmail.com>
2026-02-08 23:23:34 -0400

    fix: corrigiendo errores de extracción de datos 'noticias/lanacion-marca'
    - Corregimos los errores por no encontrar los datos.
    - También cambiamos las 'anotaciones' de las funciones.
    - Agregamos validaciones para evitar que se produzca algún error por no
    encontrar una etiqueta.

--- scraper/noticias/pages/lanacion.py
+++ scraper/noticias/pages/lanacion.py
@@ -1,16 +1,65 @@
-from typing import List
-from typing import Dict
-from typing import NewType
+import pprint
 
+from typing import TypeVar
+
+from bs4.element import Tag
 from bs4 import BeautifulSoup
 
 
-HTMLParsed = NewType("HTMLParsed", BeautifulSoup)
+HTMLParsed = TypeVar("HTMLParsed", bound = BeautifulSoup)
+
+
+def get_url(list_url: list[Tag]) -> str | None:
+	if not list_url:
+		return None
+	
+	if not isinstance(list_url[0], Tag):
+		return None
+	
+	url = list_url[0]
+	return url.get('href')
+
+
+def clean_title(title: str) -> str:
+	return title.strip().replace("\n", "").replace("  ", "")
+
+def get_title(list_title: list[Tag]) -> str | None:
+	if not list_title:
+		return None
+	if not isinstance(list_title[0], Tag):
+		return None
+
+	title = list_title[0]
+	return clean_title(title.get_text())
+
+
+def get_url_image(list_img: list[Tag]) -> str | None:
+	if not list_img:
+		return None
+	if not isinstance(list_img[0], Tag):
+		return None
+
+	img = list_img[0]
+	return img.get('src')
+
+
+def clean_date(time: str) -> str:
+	return time.strip().replace("\n", "")
+
+def get_date(list_time: list[Tag]) -> str | None:
+	if not list_time:
+		return None
+	if not isinstance(list_time[0], Tag):
+		return None
+
+	time = list_time[0]
+	return clean_date(time.get_text())
+
 
 class LaNacionNoticias:
 
 	@classmethod
-	def scrap(cls, html_parsed: HTMLParsed | None = None, url_root: str = "") -> str | List[Dict[str, str]]:
+	def scrap(cls, html_parsed: HTMLParsed | None = None, url_root: str = "") -> None | list[dict[str, str]]:
 		if html_parsed is None:
 			return None
 
@@ -20,27 +69,21 @@ class LaNacionNoticias:
 			return None
 
 		notas = []
-
 		for articulo in articulos.find_all('article', class_="mod-article"):
-			url = articulo.css.select('div[class="content-media"] a')[0].get('href')
-			imagen = articulo.css.select('div[class="content-media"] img[class="com-image"]')[0].get('src')
-			fecha = articulo.css.select('section[class="mod-description"] time')[0].string.strip()
-			titulo = [
-				articulo.css.select('h2[class*="com-title"] a[class="com-link"]')[0].string.strip()
-				if articulo.css.select('h2[class*="com-title"] a[class="com-link"]')[0].string
-				else [
-					str(articulo.css.select('h2[class*="com-title"] a[class="com-link"]')[0].contents[0]), 
-					articulo.css.select('h2[class*="com-title"] a[class="com-link"]')[0].contents[1]
-				]
-			][0]
-			url_link = articulo.css.select('h2[class*="com-title"] a[class="com-link"]')[0].get("href")
+			url = get_url(articulo.css.select('div[class="content-media"] a'))
+			imagen = get_url_image(articulo.css.select('div[class="content-media"] img[class*="image"]'))
+			fecha = get_date(articulo.css.select('section[class="mod-description"] time'))
+			
+			titulo = get_title(
+				articulo.css.select('h2[class*="com-title"] a[class="com-link"]')
+			)
 
 			notas.append({
 				'titulo': titulo,
 				'url': f"{url_root}{url[1:]}",
+				'url_link': url,
 				'imagen': imagen,
-				'fecha': fecha,
-				'url_link': url_link,
+				'fecha': fecha
 			})
 
 		return notas
--- scraper/noticias/pages/marca.py
+++ scraper/noticias/pages/marca.py
@@ -1,34 +1,52 @@
-from typing import List
-from typing import Dict
-from typing import NewType
+from typing import TypeVar
 
+from bs4.element import Tag
 from bs4 import BeautifulSoup
 
+HTMLParsed = TypeVar("HTMLParsed", bound=BeautifulSoup)
 
-HTMLParsed = NewType("HTMLParsed", BeautifulSoup)
+
+def get_img(img: Tag | None) -> str | None:
+	return img.get('src') if isinstance(img, Tag) else None
+
+def get_meta(meta: Tag | None) -> str | None:
+	return meta.get_text().strip() if isinstance(meta, Tag) else None
+
+def get_title(title: Tag | None) -> str | None:
+	return title.get_text().strip() if isinstance(title, Tag) else None
+
+def get_url(url: Tag | None) -> str | None:
+	return url.get('href') if isinstance(url, Tag) else None
+
+def get_author(author: Tag | None) -> str | None:
+	if not isinstance(author, Tag):
+		return None
+
+	name = author.next_sibling.strip()
+	return name if name else None
 
 class MarcaNoticias:
 
 	@classmethod
-	def scrap(cls, html_parsed: HTMLParsed | None = None) -> str | List[Dict[str, str]]:
+	def scrap(cls, html_parsed: HTMLParsed | None = None) -> str | list[dict[str, str]]:
 
-		if html_parsed is None:
+		if not html_parsed:
 			return None
 
 		articulos = html_parsed.find('div', class_="ue-l-cover-grid__column size8of12")
 
-
-		if articulos is None:
+		if not articulos:
 			return None
 
 		notas = []
-		for articulo in articulos.css.select('div[class*="ue-l-cover-grid__unit"] article[class*="ue-c-cover-content"]'):
-			imagen = articulo.find('img').get('src')
-			meta = articulo.find('span', class_="ue-c-cover-content__kicker").string.strip()
-			url = articulo.find('a', class_="ue-c-cover-content__link").get('href')
-			titulo = articulo.find('h2', class_="ue-c-cover-content__headline").string.strip()
-			autor = articulo.find('span', class_='hidden-content').next_sibling.strip()
 
+		for articulo in articulos.css.select('div[class*="ue-l-cover-grid__unit"] article[class*="ue-c-cover-content"]'):			
+			imagen = get_img(articulo.find('img'))
+			meta = get_meta(articulo.find('span', class_="ue-c-cover-content__kicker"))
+			url = get_url(articulo.find('a', class_="ue-c-cover-content__link"))
+			titulo = get_title(articulo.find('h2', class_="ue-c-cover-content__headline"))
+			autor = get_author(articulo.find('span', class_='hidden-content'))
+			
 			notas.append({
 				'titulo': titulo,
 				'url': url,
@@ -36,5 +54,6 @@ class MarcaNoticias:
 				'meta': meta,
 				'autor': autor,
 			})
+
 
 		return notas
