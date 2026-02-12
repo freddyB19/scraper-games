352311549e1e64069e6573e1b6a3f2d6c5165d6c
freddyB19 <freddyybolivar2013@gmail.com>
2026-02-12 18:42:27 -0400

    feat: agregando funciones asíncronas de [read-file/web, run_task]

--- scraper/utils/main.py
+++ scraper/utils/main.py
@@ -1,10 +1,13 @@
-import logging
+import logging, asyncio
 
 from typing import Union, TypeVar, Dict, Any
 
 from lxml import html, etree
 
+import httpx
 from httpx import Client, ConnectError
+
+import aiofiles
 
 from bs4 import BeautifulSoup
 
@@ -44,17 +47,57 @@ class ReadFromWeb:
 			return None
 
 
+MAX_REQUEST = 10
+MAX_CONCURRENT_REQUEST = 5
+class AsyncReadFromWeb:
+	_SEMAPHORE = asyncio.Semaphore(MAX_CONCURRENT_REQUEST)
+	_LIMITS = httpx.Limits(
+		max_keepalive_connections = MAX_CONCURRENT_REQUEST, # límite de conexiones "activas" permitidas 
+		max_connections = MAX_REQUEST # límite de conexiones permitidas
+	)
+	_TIMEOUT = 10.0
+	_HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36  (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+	
+	@classmethod
+	async def read(cls, url:str) -> HTMLParsed | None:
+		async with cls._SEMAPHORE:
+			try:
+				async with httpx.AsyncClient(limits = cls._LIMITS, headers = cls._HEADERS) as client:
+				 	response = await client.get(url, timeout=cls._TIMEOUT)
+
+				if response.status_code == STATUS_CODE['OK']:
+					data = html.fromstring(response.text)
+
+					html_parsed = BeautifulSoup( html.tostring(data), 'lxml')
+					
+					return html_parsed
+				
+				return None
+			except ConnectError as e:
+				logging.error("Error en la extracción", extra = {"error": str(e), "url": url})
+				return None
+
+
 class ReadFromFile:
 
 	@classmethod
 	def read(cls, path_file):
-		data: HTMLParsed | None = None
 		with open(path_file, 'r') as file:
 			file = file.read()
 
 			data = BeautifulSoup( file, 'lxml')
 
 		return data
+
+
+class AsyncReadFromFile:
+
+	@classmethod
+	async def read(cls, path_file):
+		async with aiofiles.open(path_file, 'r') as file:
+			file = await file.read()
+
+		return BeautifulSoup( file, 'lxml')
 
 
 class DownloadFile:
@@ -70,4 +113,8 @@ class DownloadFile:
 		return {"state": True, "data": html}
 		
 
-		
+async def run_task(func, *args):	
+	loop = asyncio.get_running_loop()
+	content = await loop.run_in_executor(None, func, *args)
+
+	return content
