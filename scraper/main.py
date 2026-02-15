4a3200a1077507972cfecf5e7b1544c0820dd559
freddyB19 <freddyybolivar2013@gmail.com>
2026-02-14 22:27:59 -0400

    feat: agregando asícronismo al main principal

--- scraper/main.py
+++ scraper/main.py
@@ -1,21 +1,38 @@
-import os
-import json
+import os, json, asyncio
+
+import httpx
+
+from utils.main import async_read_from_web, ASYNC_CLIENT_CONFIG
 
 from lol.main import ScraperLOL
 from easport.main import ScraperEASport
 from noticias.main import ScraperNoticias
-
-
+from iracing.main import scraper as async_iracing_scraper
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 
-def main():
+async def main():
+	limits = ASYNC_CLIENT_CONFIG["_LIMITS"]
+	headers = ASYNC_CLIENT_CONFIG["_HEADERS"]
+	async with httpx.AsyncClient(limits = limits, headers = headers) as client:
+		tasks = [
+			ScraperLOL.async_scraper(),
+			ScraperEASport.async_scraper(),
+			ScraperNoticias.async_scraper(),
+			async_iracing_scraper()
+		]
+
+		lol, easport, news, iracing = await asyncio.gather(
+			*tasks, 
+			return_exceptions = True
+		)
+
 
 	results = [
-		{'pagina': {'nombre': 'lol', 'info': ScraperLOL.scraper()}},
-		{'pagina': {'nombre': 'easport', 'info': ScraperEASport.scraper()}},
-		{'pagina': {'nombre': 'noticias', 'info': ScraperNoticias.scraper()}},
-
+		{'page': {'name': 'lol', 'data': lol}},
+		{'page': {'name': 'easport', 'data': easport}},
+		{'page': {'name': 'noticias', 'data': news}},
+		{'page': {'name': 'iracing', 'data': iracing}},
 	]
 
 	with open(os.path.join(BASE, 'result.json'), 'w+') as file:
@@ -23,4 +40,4 @@ def main():
 	
 
 if __name__ == '__main__':
-	main()
+	asyncio.run(main())
