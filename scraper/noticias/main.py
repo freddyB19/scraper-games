3c723531e9a4fc7569f42e3af139988bbd34a471
freddyB19 <freddyybolivar2013@gmail.com>
2026-02-23 02:13:02 -0400

    style: eliminando soporte sync del 'scraper/noticias'

--- scraper/noticias/main.py
+++ scraper/noticias/main.py
@@ -33,74 +33,34 @@ URL = {
 	'wired_robots': 'https://es.wired.com/tag/robots',
 }
 
-async def asyn_marca(client: AsyncClient) -> ScraperResult:
+async def async_marca(client: AsyncClient) -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['marca'])
 	return await run_task(MarcaNoticias.scrap, content_html)
 
-async def asyn_lanacion(client: AsyncClient, url_root:str = 'https://www.lanacion.com.ar/') -> ScraperResult:
+async def async_lanacion(client: AsyncClient, url_root:str = 'https://www.lanacion.com.ar/') -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['lanacion'])
 	return await run_task(LaNacionNoticias.scrap, content_html, url_root)
 
-async def asyn_lanacion_tecnologia(client: AsyncClient, url_root:str = 'https://www.lanacion.com.ar/') -> ScraperResult:
+async def async_lanacion_tecnologia(client: AsyncClient, url_root:str = 'https://www.lanacion.com.ar/') -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['lanacion_tecnologia'])
 	return await run_task(LaNacionNoticias.scrap, content_html, url_root)
 
-async def asyn_wired_espacio(client: AsyncClient) -> ScraperResult:
+async def async_wired_espacio(client: AsyncClient) -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['wired_espacio'])
 	return await run_task(get_news, content_html, URL['wired_espacio'])
 
-async def asyn_wired_biotecnologia(client: AsyncClient) -> ScraperResult:
+async def async_wired_biotecnologia(client: AsyncClient) -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['wired_biotecnologia'])
 	return await run_task(get_news, content_html, URL['wired_biotecnologia'])
 
-async def asyn_wired_neurociencia(client: AsyncClient) -> ScraperResult:
+async def async_wired_neurociencia(client: AsyncClient) -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['wired_neurociencia'])
 	return await run_task(get_news, content_html, URL['wired_neurociencia'])
 
-async def asyn_wired_robots(client: AsyncClient) -> ScraperResult:
+async def async_wired_robots(client: AsyncClient) -> ScraperResult:
 	content_html = await async_read_from_web(client, URL['wired_robots'])
 	return await run_task(get_robots_news, content_html, URL["wired_robots"])
 
-def marca() -> ScraperResult:
-	return MarcaNoticias.scrap( 
-		html_parsed = ReadFromWeb.read(URL['marca'])
-	)
-
-def lanacion() -> ScraperResult:
-	return LaNacionNoticias.scrap(
-		html_parsed = ReadFromWeb.read(URL['lanacion']),
-		url_root = 'https://www.lanacion.com.ar/'
-	)
-
-def lanacion_tecnologia() -> ScraperResult:
-	return LaNacionNoticias.scrap(
-		html_parsed = ReadFromWeb.read(URL['lanacion_tecnologia']),
-		url_root = 'https://www.lanacion.com.ar/'
-	)
-
-def wired_robots() -> ScraperResult:
-	return get_robots_news(
-		html_parsed = ReadFromWeb.read(URL["wired_robots"]),
-		url_root = URL["wired_robots"]
-	)
-
-def wired_neurociencia() -> ScraperResult:
-	return get_news(
-		html_parsed = ReadFromWeb.read(URL["wired_neurociencia"]), 
-		url_root = URL["wired_neurociencia"]
-	)
-
-def wired_biotecnologia() -> ScraperResult:
-	return get_news(
-		html_parsed = ReadFromWeb.read(URL["wired_biotecnologia"]), 
-		url_root = URL["wired_biotecnologia"]
-	)
-
-def wired_espacio() -> ScraperResult:
-	return get_news(
-		html_parsed = ReadFromWeb.read(URL["wired_espacio"]), 
-		url_root = URL["wired_espacio"]
-	)
 
 class ScraperNoticias:
 
@@ -108,13 +68,13 @@ class ScraperNoticias:
 	async def async_scraper(cls):
 		async with httpx.AsyncClient(limits = ASYNC_CLIENT_CONFIG["_LIMITS"], headers = ASYNC_CLIENT_CONFIG["_HEADERS"]) as client:
 			tasks = [
-				asyn_marca(client),
-				asyn_lanacion(client),
-				asyn_lanacion_tecnologia(client),
-				asyn_wired_espacio(client),
-				asyn_wired_biotecnologia(client),
-				asyn_wired_neurociencia(client),
-				asyn_wired_robots(client)
+				async_marca(client),
+				async_lanacion(client),
+				async_lanacion_tecnologia(client),
+				async_wired_espacio(client),
+				async_wired_biotecnologia(client),
+				async_wired_neurociencia(client),
+				async_wired_robots(client)
 			]
 
 			results = await asyncio.gather(*tasks, return_exceptions = True)
@@ -171,44 +131,6 @@ class ScraperNoticias:
 			}
 		]
 
-	@classmethod
-	def scraper(cls) -> ScraperResult:
-
-		return [
-			{'nombre': 'marca', 'pagina': marca()},
-			{
-				'nombre': 'la nación', 
-				'pagina': lanacion(),
-				'subs': [
-					{
-						'nombre': 'la nación tecnología', 
-						'pagina': lanacion_tecnologia()
-					}
-				]
-			},
-			{
-				'nombre': 'wired',
-				'subs': [
-					{
-						'nombre': 'wired robots',
-						'pagina': wired_robots()
-					},
-					{
-						'nombre': 'wired neurociencia',
-						'pagina': wired_neurociencia()
-					},
-					{
-						'nombre': 'wired biotecnologia',
-						'pagina': wired_biotecnologia()
-					},
-					{
-						'nombre': 'wired spacio',
-						'pagina': wired_espacio()
-					},
-
-				]
-			}
-		]
 
 async def main():
 	pprint.pprint(await ScraperNoticias.async_scraper(), indent = 4)
